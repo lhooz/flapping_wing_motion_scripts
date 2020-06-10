6f85e78e32e71aa2c9815fd8effa7489b196fd8f
hao <hao.lee0019@gmail.com>
2020-06-10 17:38:50 +0100

    add transformation of euler angle to rotation vector using Scipy

--- flapping_wing_motion.py
+++ flapping_wing_motion.py
@@ -1,6 +1,7 @@
 """script for tabulated 6DOF flapping wing motion"""
 
 import numpy as np
+from scipy.spatial.transform import Rotation as R
 
 flapping_amplitude = 90
 pitching_amplitude = 90
@@ -14,29 +15,32 @@ t = np.linspace(start_time, end_time, time_series_length)
 
 phi = (flapping_amplitude / 2) * np.sin(2 * np.pi * flapping_frequency * t -
                                         np.pi / 2)
+alf = (pitching_amplitude / 2) * np.sin(2 * np.pi * flapping_frequency * t +
+                                        np.pi)
+# r = R.from_euler('zyx', [-30, -20, 0], degrees=True)
+# r = r.as_rotvec()
 
-alf = (pitching_amplitude / 2) * np.sin(2 * np.pi * flapping_frequency * t)
+r_vector = []
+for phii, alfi in zip(phi, alf):
+    euler_anglesi = [phii, alfi, 0]
+
+    roti = R.from_euler('zyx', euler_anglesi, degrees=True)
+    r_vectori = roti.as_rotvec() * 180 / np.pi
+    r_vectori = [str(r_vectori[0]), str(r_vectori[1]), str(r_vectori[2])]
+
+    r_vector.append(r_vectori)
 
 t = [str(ti) for ti in t]
-angle_z = [str(phii) for phii in phi]
-angle_y = [
-    str(alfi * np.cos(phii * np.pi / 180)) for alfi, phii in zip(alf, phi)
-]
-angle_x = [
-    str(alfi * np.sin(phii * np.pi / 180)) for alfi, phii in zip(alf, phi)
-]
-# print(phi)
 
 motion = ['1000', '(']
-for ti, angle_xi, angle_yi, angle_zi in zip(t, angle_x, angle_y, angle_z):
-    motioni = '(' + ti + ' ((0 0 0)' + '(' + ' '.join(
-        [angle_xi, angle_yi, angle_zi]) + '))'
+for ti, angle_i in zip(t, r_vector):
+    motioni = '(' + ti + ' ((0 0 0)' + '(' + ' '.join(angle_i) + ')))'
 
     motion.append(motioni)
 
 motion.append(')')
 
 # print(motion)
-with open('6DOF.dat', 'w') as f:
+with open('6DoF.dat', 'w') as f:
     for item in motion:
         f.write("%s\n" % item)
