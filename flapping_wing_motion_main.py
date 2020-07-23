c9ae8570ec24206c7d9d1f6e0770a2b6d1fef12b
hao <hao.lee0019@gmail.com>
2020-07-23 10:37:43 +0100

    refinement of input specification and added plotter for functions

--- flapping_wing_motion_main.py
+++ flapping_wing_motion_main.py
@@ -1,38 +1,41 @@
 """script for tabulated 6DOF flapping wing motion"""
 
 import numpy as np
-import matplotlib.pyplot as plt
 from kinematic_functions import smooth_kinematic_function as sm_f
 from kinematic_functions import sinusoidal_kinematic_function as si_f
-from kinematics_write import write_2d, write_3d
+from kinematics_write import kf_plotter, write_2d, write_3d
 
+# sinumation time definition and choose functions to use
 time_series_length = 10000
 start_time = 0
 end_time = 80
+use_function = 'smooth'
+# use_function = 'sinusoidal'
 
-t = np.linspace(start_time, end_time, time_series_length)
-
-# kinematic parameters for default kinematic functions
 # ----------------------------------------------
+# common kinematic parameters
 flapping_wing_frequency = 0.1
-flapping_amplitude = 80
-pitching_amplitude = 45
 
-flapping_acceleration_time_coefficient = 0.99  # between 0 and 1
-pitching_time_coefficient = 3.3  # between 0 and inf
+pitching_amplitude = 45
 
 flapping_delay_time_fraction = 0
 pitching_delay_time_fraction = 0
 
 section_location = 1
-
-# additional kinematic control parameters for mostafa_function
 # ----------------------------------------------
+# additional kinematic parameters for smooth functions
+flapping_amplitude = 60
+
+flapping_acceleration_time_coefficient = 0.99  # between 0 and 1
+pitching_time_coefficient = 3.3  # between 0 and inf
+
+# additional kinematic control parameters for sinusoidal functions
 flapping_angular_velocity_amplitude = 0.5 * 180 / np.pi
 
-flapping_acceleration_time_fraction = 0.24
-pitching_time_fraction = 0.24
+flapping_acceleration_time_fraction = 0.15
+pitching_time_fraction = 0.2
 # ----------------------------------------------
+t = np.linspace(start_time, end_time, time_series_length)
 
 kinematic_parameters_smooth = [
     flapping_wing_frequency, flapping_amplitude, pitching_amplitude,
@@ -47,14 +50,17 @@ kinematic_parameters_sinusoidal = [
     pitching_delay_time_fraction
 ]
 
-kinematic_angles = sm_f(t, kinematic_parameters_smooth)
-# kinematic_angles = si_f(t, kinematic_parameters_sinusoidal)
+if use_function == 'smooth':
+    kinematic_angles = sm_f(t, kinematic_parameters_smooth)
+elif use_function == 'sinusoidal':
+    kinematic_angles = si_f(t, kinematic_parameters_sinusoidal)
 
-# plotting kinematic_angles
+# plotting kinematic angles
 t_1st_cycle = [t1 for t1 in t if t1 <= 1 / flapping_wing_frequency]
-plt.plot(t_1st_cycle, kinematic_angles)
-plt.show()
+# angles_to_plot = ['phi', 'dphi', 'alf', 'dalf']
+angles_to_plot = ['dphi', 'alf']
 
+kf_plotter(t_1st_cycle, kinematic_angles, angles_to_plot)
 # ----------------------------------------------
 write_2d(t, section_location, flapping_wing_frequency, kinematic_angles)
 write_3d(t, flapping_wing_frequency, kinematic_angles)
--- kinematics_write.py
+++ kinematics_write.py
@@ -1,14 +1,60 @@
 """script for writing kinematic functions to openfoam format"""
 
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.spatial.transform import Rotation as R
+
+
+def kf_plotter(x_array, y_arrays, legends):
+    """
+    A helper function to make a graph
+
+    Parameters
+    ----------
+    x_array : array
+       The x data
+
+    y_arrays : nd_array
+       The y data arrays
+
+    legends : list
+       list of data name for each y array
+
+    Returns
+    -------
+    out : list
+        list of artists added
+    """
+    x_array = np.array(x_array)
+    y_arrays = np.array(y_arrays)
+
+    fig, ax = plt.subplots(1, 1)
+
+    for legend in legends:
+        if legend == 'phi':
+            ax.plot(x_array, y_arrays[:, 0], label='phi')
+        elif legend == 'alf':
+            ax.plot(x_array, y_arrays[:, 1], label='alf')
+        elif legend == 'dphi':
+            ax.plot(x_array, y_arrays[:, 2], label='dphi')
+        elif legend == 'dalf':
+            ax.plot(x_array, y_arrays[:, 3], label='dalf')
+
+    ax.set_xlabel('t (seconds)')
+    ax.set_ylabel('angle (degrees)')
+    ax.set_title('kinematics plot')
+    ax.legend()
+
+    plt.show()
+
+    return fig
+
 
 def write_2d(t, section_location, flapping_wing_frequency, kinematic_angles):
     """write kinematics data for 2d wing motion"""
-    t_1st_cycle = [t1 for t1 in t if t1 <= 1/flapping_wing_frequency]
+    t_1st_cycle = [t1 for t1 in t if t1 <= 1 / flapping_wing_frequency]
     no_of_points_per_cycle = len(t_1st_cycle)
     time_series_length = len(t)
-
 
     t_disp = []
     r_angle = []
@@ -34,7 +80,8 @@ def write_2d(t, section_location, flapping_wing_frequency, kinematic_angles):
 
     motion = [str(time_series_length), '(']
     for ti, disp_i, angle_i in zip(t, t_disp, r_angle):
-        motioni = '(' + ti + ' (('+ ' '.join(disp_i)+')' + '(' + ' '.join(angle_i) + ')))'
+        motioni = '(' + ti + ' ((' + ' '.join(disp_i) + ')' + '(' + ' '.join(
+            angle_i) + ')))'
 
         motion.append(motioni)
 
@@ -47,15 +94,16 @@ def write_2d(t, section_location, flapping_wing_frequency, kinematic_angles):
 
 def write_3d(t, flapping_wing_frequency, kinematic_angles):
     """write kinematics data for 3d wing motion"""
-    t_1st_cycle = [t1 for t1 in t if t1 <= 1/flapping_wing_frequency]
+    t_1st_cycle = [t1 for t1 in t if t1 <= 1 / flapping_wing_frequency]
     no_of_points_per_cycle = len(t_1st_cycle)
     time_series_length = len(t)
-
 
     r_angle = []
     for i in range(time_series_length):
         i_moded = np.mod(i, no_of_points_per_cycle)
-        kinematic_anglesi = [kinematic_angles[i_moded][0], kinematic_angles[i_moded][1], 0]
+        kinematic_anglesi = [
+            kinematic_angles[i_moded][0], kinematic_angles[i_moded][1], 0
+        ]
 
         roti = R.from_euler('YXZ', kinematic_anglesi, degrees=True)
 
